e8e30c54625089f49f910793aa0c77140339f7b6
heypolygot <helloajaydey@gmail.com>
2025-05-23 19:56:16 +0530

    modified code

--- singly_list/creation/create_linkedlist.c
+++ singly_list/creation/create_linkedlist.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 
+//defining the structure of the node
 typedef struct node{
 	int value;
 	struct node *next;
-}Node;
+}SNode;
+
+
+//defining the structure of the list.
+typedef struct list{
+	SNode *head;
+	int len;
+}SList;
 
 void main() {
-	Node a,b,c;
+	SList mylist;
+	SNode a,b,c;
 
+	mylist.head = &a;
 	a.value=5;
+	mylist.len++;
 	a.next=&b;
 
 	b.value=10;
+	mylist.len++;
 	b.next=&c;
 
 	c.value=15;
-	c.next=NULL;	
+	mylist.len++;
+	c.next=NULL;
+
+	printf("%d\n", mylist.len);
+	printf("value of 2nd node = %d", mylist.head->next->value);
 }
